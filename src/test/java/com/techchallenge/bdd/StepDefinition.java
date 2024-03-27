adbbec30864bd8a1d18d4cea40f58d1de80897d3
danielcorreaa <daniel.cor@outlook.com>
2024-03-27 20:04:48 -0300

    ajustes finais

--- src/test/java/com/techchallenge/bdd/StepDefinition.java
+++ src/test/java/com/techchallenge/bdd/StepDefinition.java
@@ -29,9 +29,9 @@ public class StepDefinition {
 
     OrderHelper helper = new OrderHelper();
 
-    private String ENDPOINT_ORDERS = "http://localhost:8085/orders/api/v1";
-    private String ENDPOINT_PRODUCTS = "http://localhost:8087/products/api/v1";
-    private String ENDPOINT_CUSTOMERS = "http://localhost:8083/customers/api/v1";
+    private String ENDPOINT_ORDERS = "http://localhost:8085/tech-challenge-orders/orders/api/v1";
+    private String ENDPOINT_PRODUCTS = "http://localhost:8087/tech-challenge-product/products/api/v1";
+    private String ENDPOINT_CUSTOMERS = "http://localhost:8083/tech-challenge-customer/customers/api/v1";
 
     @Dado("que quero inciar um pedido")
     public void que_quero_inciar_um_pedido() {
